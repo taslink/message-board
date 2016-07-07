a517c9009d3bfa4661d2979e660dc08bc3f6f34c
hiroyuki <ikuyorihato@gmail.com>
2016-07-07 07:23:20 +0000

    _form template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'messages#index'
-  resources :messages , only: [:create]
+  resources :messages , except: [:index, :new]
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -56,4 +57,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
